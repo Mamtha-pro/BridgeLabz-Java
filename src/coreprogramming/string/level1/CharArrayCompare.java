99b2d0a8c6ad867dfe622195ab09cb854b22bf3a
Mamatha-Rai-Pro <179235094+Mamtha-pro@users.noreply.github.com>
2026-06-17 00:18:21 +0530

    string program

--- src/coreprogramming/string/level1/CharArrayCompare.java
+++ src/coreprogramming/string/level1/CharArrayCompare.java
@@ -1,4 +1,26 @@
 package coreprogramming.string.level1;
 
 public class CharArrayCompare {
+    public static void main (String[] args){
+        String name = "Mamatha Rai";
+        char []arr=new char[name.length()];
+        for(int i =0; i<name.length();i++){
+            arr[i] = name.charAt(i);
+
+        }
+        char []arr2 = name.toCharArray();
+
+
+        for(int i =0;i<arr.length;i++){
+            if(arr.length != arr2.length){
+                System.out.print("false");
+            }
+                else if(arr[i] != arr2[i]){
+                System.out.print("false");
+            }else{
+                System.out.print("true");
+            }
+        }
+
+    }
 }
